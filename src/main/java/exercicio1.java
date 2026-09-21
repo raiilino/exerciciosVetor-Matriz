8ea1a855ae92b9294c37a8877ce04e8496fb1ad7
raiilino <raissaoliveiralino@gmail.com>
2026-09-21 16:00:47 -0300

    exercicio1.java

---
+++ src/main/java/exercicio1.java
@@ -0,0 +1,13 @@
+class Main {
+    public static void main(String[] args) {
+        int[] numeros = new int[5];
+        numeros[0] = 67;
+        numeros[1] = 20;
+        numeros[2] = 89;
+        numeros[3] = 29;
+        numeros[4] = 16;
+        for (int i = 0; i < 5; i++) {
+            System.out.println(numeros[i]);
+        }
+    }
+}
